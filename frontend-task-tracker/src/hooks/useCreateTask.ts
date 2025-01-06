18f1ca920d5a73730678e884ef2d387b2cba0790
Starlex <alexsiviper@gmail.com>
2025-01-06 09:43:05 +0300

    feat: updated create task hook

--- frontend-task-tracker/src/hooks/useCreateTask.ts
+++ frontend-task-tracker/src/hooks/useCreateTask.ts
@@ -4,7 +4,7 @@ import type { TaskAPIData } from '@/types/TaskAPIData.interface';
 
 export const useCreateTask = async (data: Omit<Task, '_id'>): Promise<TaskAPIData | null> => {
   try {
-    const response = await axios.post<Task>('tasks/', data);
+    const response = await axios.post<Task>('tasks/create', data);
     return { data: response.data, code: response.status };
   } catch (e) {
     console.error(e);
