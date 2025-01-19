6f272a1b62f5d3016d1b968de26bfd210096c9b0
Starlex <alexsiviper@gmail.com>
2025-01-19 15:07:44 +0300

    feat: added bottom gradient for header

--- frontend-task-tracker/src/layout/TheHeader.vue
+++ frontend-task-tracker/src/layout/TheHeader.vue
@@ -1,9 +1,14 @@
 <template>
-  <header class="tw-sticky tw-top-0 tw-z-10 tw-bg-[--bg-main] dark:tw-bg-[var(--bg-main-darkT)]">
+  <header
+    class="tw-sticky tw-pt-8 tw-top-0 tw-z-10 tw-bg-[--bg-main] dark:tw-bg-[var(--bg-main-darkT)]"
+  >
     <div class="tw-flex tw-flex-col tw-gap-8 tw-items-center">
       <TheLogo />
       <ActionMenu />
     </div>
+    <div
+      class="tw-absolute tw-inset-x-0 tw-top-full tw-h-10 tw-bg-gradient-to-b tw-from-[var(--bg-main)] tw-to-transparent dark:tw-from-[var(--bg-main-darkT)]"
+    ></div>
   </header>
 </template>
 
